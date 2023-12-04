478d5988308519deeaf15c68613009b1517eed4b
Данила Ведешкин <danilavedeskin@MacBook-Pro-Danila.local>
2023-12-04 04:01:18 +0300

    some cities

--- microblog/flask/myblueprint/main.py
+++ microblog/flask/myblueprint/main.py
@@ -9,6 +9,11 @@ main_bp = Blueprint(
     'main_blueprint', __name__,
 )
 
+city_dict = {
+    "Moscow": 'Москва',
+    "London": 'Лондон',
+    "Tokyo": 'Токио'
+}
 
 @main_bp.route('/', methods=['POST', 'GET'])
 @main_bp.route('/main', methods=['POST', 'GET'])
@@ -42,19 +47,57 @@ def index():
     )
 
 
-@main_bp.route('/weather_cast')
-def weather_cast():
-    # ПОГОДА
+def weather_cast_post(request):
 
-    city = input('Введите город: ')
 
-    response = requests.get(
-        "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&lang=ru&units=metric&appid=" + API_KEY)
+    city = request.form.get('city')
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&lang=ru&units=metric&appid={API_KEY}"
+    response = requests.get(url=url)
     data = response.json()
     cur_temp = data["main"]["temp"]
     humidity = data["main"]["humidity"]
     pressure = data["main"]["pressure"]
     wind = data["wind"]["speed"]
+    print(response.json())
+
+    sunrise_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
+    sunset_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunset"])
+    length_of_the_day = datetime.datetime.fromtimestamp(data["sys"]["sunset"]) - datetime.datetime.fromtimestamp(
+        data["sys"]["sunrise"])
+    print("Обработка POST-запроса")
+    context = {
+        'cur_temp': cur_temp,
+        'humidity': humidity,
+        'pressure': pressure,
+        'wind': wind,
+        'length_of_the_day': length_of_the_day,
+        'sunrise_timestamp': sunrise_timestamp,
+        'sunset_timestamp': sunset_timestamp,
+        'city_dict': city_dict,
+        'selected_city': city
+    }
+    return render_template(
+        "weather.html",
+        context=context
+    )
+
+
+@main_bp.route('/weather_cast', methods=['POST', 'GET'])
+def weather_cast():
+
+    if request.method == 'POST':
+        return weather_cast_post(request)
+    # ПОГОДА
+
+    city = "Moscow"
+    url = f"http://api.openweathermap.org/data/2.5/weather?q={city}&lang=ru&units=metric&appid={API_KEY}"
+    response = requests.get(url=url)
+    data = response.json()
+    cur_temp = data["main"]["temp"]
+    humidity = data["main"]["humidity"]
+    pressure = data["main"]["pressure"]
+    wind = data["wind"]["speed"]
+    print(response.json())
 
     sunrise_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunrise"])
     sunset_timestamp = datetime.datetime.fromtimestamp(data["sys"]["sunset"])
@@ -83,6 +126,7 @@ def weather_cast():
         'sunrise_timestamp': sunrise_timestamp,
         'sunset_timestamp': sunset_timestamp,
         'weather': weather,
+        'city_dict': city_dict,
         'title': "Home",
 
     }
